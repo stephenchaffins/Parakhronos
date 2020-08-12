56cba35e72e83a35b3ca533a4cf700498ed201d0
stephenchaffins <stephenchaffins@gmail.com>
2020-08-12 13:25:39 -0400

    First lines of code for the App.

--- parakhronos.sh
+++ parakhronos.sh
@@ -7,5 +7,31 @@
 # @Project: Parakhronos
 # @Filename: parakhronos.sh
 # @Last modified by:   schaffins
-# @Last modified time: 2020-08-12T06:10:25-04:00
+# @Last modified time: 2020-08-12T13:25:06-04:00
 # -----------------------------------------------------------------------------
+
+
+# -----------------------------------------------------------------------------
+# Setting some basic variables just to get us started. This includes the list
+# of users that will be used. A single user name can be used. This is ran with
+# something like /parakhronos.sh USERNAME USERNAME2.
+# -----------------------------------------------------------------------------
+
+IFS=$'\n'
+masteruserlist=( "$@" )
+
+# -----------------------------------------------------------------------------
+# Find out what kind of server it is. VDS master, VDS client, or cpanel.
+# -----------------------------------------------------------------------------
+
+if [[ ! -f /usr/local/cpanel/cpanel ]] || [[ -d /sphera ]] ; then
+  echo "This is VDS Master Server"
+elif [[ ! -f /usr/local/cpanel/cpanel ]] || [[ ! -d /sphera ]]; then
+  echo "This is a VDS User Account"
+elif [[ -f /usr/local/cpanel/cpanel ]]; then
+  echo "This is a cPanel Server"
+else
+  echo "WHAT IS THIS SERVER?!"
+fi
+
+exit 0
