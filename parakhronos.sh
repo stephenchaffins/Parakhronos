0d192433a0f8e6b1902e736e7e094183c318b00d
stephenchaffins <stephenchaffins@gmail.com>
2020-08-12 16:53:52 -0400

    Logging is sorted.

--- parakhronos.sh
+++ parakhronos.sh
@@ -7,7 +7,7 @@
 # @Project: Parakhronos
 # @Filename: parakhronos.sh
 # @Last modified by:   schaffins
-# @Last modified time: 2020-08-12T16:51:51-04:00
+# @Last modified time: 2020-08-12T16:53:41-04:00
 # -----------------------------------------------------------------------------
 
 
