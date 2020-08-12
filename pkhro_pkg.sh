9528a7279b93f29b997d4b4d1f5d68bdf1368523
stephenchaffins <stephenchaffins@gmail.com>
2020-08-12 09:23:10 -0400

    Removing workdir.

--- pkhro_pkg.sh
+++ pkhro_pkg.sh
@@ -7,7 +7,7 @@
 # @Project: Parakhronos
 # @Filename: parakhronos.sh
 # @Last modified by:   schaffins
-# @Last modified time: 2020-08-12T09:21:30-04:00
+# @Last modified time: 2020-08-12T09:22:57-04:00
 # -----------------------------------------------------------------------------
 
 exec 2>> /var/log/parakhronos.log
