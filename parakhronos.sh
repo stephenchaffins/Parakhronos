7324c37b57cc951b1cfc3aaf2a6f5626a749403c
Stephen Chaffins <stephenchaffins@gmail.com>
2020-08-17 23:21:58 -0400

    Recommiting changes, with repair that permits all the changes. It was literally just a missing quote.

--- parakhronos.sh
+++ parakhronos.sh
@@ -7,7 +7,7 @@
 # @Project: Parakhronos
 # @Filename: parakhronos.sh
 # @Last modified by:   schaffins
-# @Last modified time: 2020-08-12T17:43:42-04:00
+# @Last modified time: 2020-08-17T00:21:44-04:00
 # -----------------------------------------------------------------------------
 
 
@@ -28,11 +28,6 @@ function dye()
    kill -s TERM $TOP_PID
 }
 
-echo "Type the full hostname of the destination/cPanel server, followed by [ENTER]:"
-read fulldesthost
-
-echo "Type the full path of the SSH key you wish to use, followed by [ENTER]:"
-read fullkeythost
 
 # -----------------------------------------------------------------------------
 # Some basic checks to create directories and files necessary to
@@ -45,14 +40,40 @@ mkdir -p /root/
 mkdir -p /var/log/parakhronos_logs/
 
 # Logging
-
 exec 2> /var/log/parakhronos_logs/stderr.log 1> >(tee -i /var/log/parakhronos_logs/stdout.log)
 
-# Download the appropriate script for the server type.
+# -----------------------------------------------------------------------------
+# Checking which server type is running, cPanel or VDS. Once determined this
+# will download the appropriate pkg or restore script. If downloading the pkg
+# script, it will also ask if you want to have it migrate to a destination
+# server once packaged.
+# -----------------------------------------------------------------------------
 if [[ ! -f /usr/local/cpanel/cpanel ]]; then
   wget -q --no-check-certificate --no-cache --no-cookie https://raw.githubusercontent.com/stephenchaffins/Parakhronos/master/pkhro_pkg.sh -O /root/pkhro_pkg.sh
   chmod 755 /root/pkhro_pkg.sh
-elif [[ -f /usr/local/cpanel/cpanel ]]; then
+          while :; do
+            echo "Would you like this script to auto-copy the packaged account file to the destination server? [Y]es/[n]o"
+            read shouldrsync
+            echo
+            if [ "$shouldrsync" = "Y" ] || [ "$shouldrsync" = "Yes" ] || [ "$shouldrsync" = "y" ] || [ "$shouldrsync" = "yes" ]; then
+              echo "Type the full hostname of the destination/cPanel server, followed by [ENTER]:";
+              read fulldesthost
+              echo
+
+              echo "Type the full path of the SSH key you wish to use, followed by [ENTER]:"
+              read fullkeythost
+              shouldrsync="10"
+              echo
+              break
+            elif [ "$shouldrsync" = "N" ] || [ "$shouldrsync" = "No" ] || [ "$shouldrsync" = "n" ] || [ "$shouldrsync" = "No" ]; then
+              echo "No problemo, no rsyncing at the end."; echo;
+              shouldrsync="11"
+              break
+            else
+              echo -e "\e[33m\e[1m Unrecognizable Response, Please enter [Y]es or [N]o. \e[0m";echo;echo;
+            fi
+          done
+]elif [[ -f /usr/local/cpanel/cpanel ]]; then
   wget -q --no-check-certificate --no-cache --no-cookie https://raw.githubusercontent.com/stephenchaffins/Parakhronos/master/pkhro_restore.sh -O /root/pkhro_restore.sh
   chmod 755 /root/pkhro_restore.sh
 else
@@ -62,26 +83,43 @@ fi
 
 
 # -----------------------------------------------------------------------------
-# Find out what kind of server it is. VDS master, VDS client, or cpanel. Then
-# kick off the appropriate package or restore script.
+# Again checking which server this is (probably could store that as a variable)
+# in the previous check). Then, this makes some directories, copies the script
+# and runs it. If VDS was chosen, this is the part that scp's to the
+# destination server. The cPanel version generates a unique password for each
+# account, restores then displays it after it restores the accounts.
+# Then this cleans up everything except the original pkg file.
 # -----------------------------------------------------------------------------
+
+echo -e "\e[93m -------------------------------------------------------------------------------- \e[0m"
+echo -e "\e[93m ###################### \e[91m\e[1mStaring Migration Process Now \e[0m\e[93m########################### \e[0m"
+echo -e "\e[93m -------------------------------------------------------------------------------- \e[0m"
 
 if [[ ! -f /usr/local/cpanel/cpanel ]] ; then
   for i in "${masteruserlist[@]}"
   do
     echo -e "\e[33m\e[1m Making $i root directory... \e[0m";sleep 1; echo
-    eval mkdir -p "~$i/root/migration_scripts"
+    eval mkdir -p ~"$i/root/migration_scripts"
     echo -e "\e[33m\e[1m Copying script to $i root directory... \e[0m";sleep 1; echo
-    eval cp -av /root/pkhro_pkg.sh "~$i/root/migration_scripts/"
+    eval cp -av /root/pkhro_pkg.sh ~"$i/root/migration_scripts/"
     echo -e "\e[33m\e[1m Chowning root directory to $i ownership... \e[0m";sleep 1; echo
-    eval chown $i: -R "~$i/root/migration_scripts/"
+    eval chown $i: -R ~"$i/root/migration_scripts/"
     echo -e "\e[33m\e[1m Running pkhro_pkg.sh inside of $i VDS... \e[0m";sleep 1; echo
     su - $i -c 'cd /root/migration_scripts/; /bin/bash pkhro_pkg.sh'
-    echo -e "\e[33m\e[1m Rsyncing $i to vmcp14... \e[0m";sleep 1; echo
-    eval scp -v -P 1022 -i "$fullkeythost" ~$i/root/parakhronos_restore_$i.tar root@"$fulldesthost":/root/
-    su - $i -c 'rm -rf /root/migration_scripts;'
-    eval rm -f /root/pkhro_pkg.sh
-    echo
+        if [ "$shouldrsync" -eq "10" ]; then
+        echo -e "\e[33m\e[1m Rsyncing $i to vmcp14... \e[0m";sleep 1; echo
+        eval scp -v -P 1022 -i "$fullkeythost" ~"$i/root/parakhronos_restore_$i.tar" root@"$fulldesthost":/root/
+            if [[ $? -eq 0 ]]; then
+              echo -e "\e[33m\e[1m Rsyncing $i to vmcp14 was success! \e[0m";
+            else
+            echo -e "\e[1m\e[41m Rsync Failure!! \e[0m";echo
+            fi
+        fi
+    eval rm -rf ~"$i/root/migration_scripts"
+    eval rm -f ~"$i/root/pkhro_pkg.sh"
+    echo -e "\e[93m -------------------------------------------------------------------------------- \e[0m"
+    echo -e "\e[93m ############################ \e[91m\e[1mAccount "$i" Migrated \e[0m\e[93m################################## \e[0m"
+    echo -e "\e[93m -------------------------------------------------------------------------------- \e[0m"
   done
 elif [[ -f /usr/local/cpanel/cpanel ]]; then
   for i in "${masteruserlist[@]}"
@@ -106,5 +144,9 @@ else
 fi
 
 rm -f /root/pkhro_*
+
+echo -e "\e[93m -------------------------------------------------------------------------------- \e[0m"
+echo -e "\e[93m ############################## \e[91m\e[1mMigration Complete!\e[0m\e[93m ################################## \e[0m"
+echo -e "\e[93m -------------------------------------------------------------------------------- \e[0m"
 
 exit 0
